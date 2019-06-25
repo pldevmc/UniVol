5c0ec7a74faf93fe399143684a1ca851d6f66365
Joasia <Joasia@LAPTOP-SOVMV3M5>
2019-06-26 00:16:14 +0200

    Updated

---
+++ mcmod/src/main/java/com/pldevmc/univol/util/Reference.java
@@ -0,0 +1,13 @@
+package com.pldevmc.univol.util;
+
+public class Reference {
+	
+	public static final String MOD_ID = "univol";
+	public static final String NAME = "Universal Voltage";
+	public static final String VERSION = "1.0";
+	public static final String ACCEPTED_VERSION = "(1.12.2)";
+	public static final String CLIENT_PROXY_CLASS = "univol";
+	public static final String COMMON_PROXY_CLASS = "univol";
+	
+	
+}
